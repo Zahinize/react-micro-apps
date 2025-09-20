140c231e9653a43c62587d67ee0cf4110db30eac
Zahin Omar Alwa <zahin.alwa@gmail.com>
2025-09-20 23:20:10 +0530

    [Changes] Improved page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import lovelogo from './assets/love.png';
-import './App.css';
 import Todo from './Todo';
 import Counter from './Counter';
 import Card from './Card';
@@ -11,15 +10,13 @@ import ImageCarousel from './components/ImageCarousel';
 function App() {
 
   return (
-    <div className='root-container d-flex d-column row-gap-4x'>
-      <header>
-        <h1 className='mb-20 mt-0'>
-          React-Interview
-        </h1>
-        <p className='mt-0 mb-10'>A collection of React components & apps for your interview preparation.</p>
-        <p className='mt-0 mb-20'>No frameworks. Pure vanilla React.</p>
+    <div className='root-container d-flex d-column'>
+      <header className='d-flex d-x-center d-y-center mb-50'>
+        <h1>React-Interview</h1>
       </header>
-      <div className='component-wrapper d-flex d-wrap d-y-start col-gap-x'>
+      <p className='mt-0 mb-10'>A collection of React components & apps for your interview preparation.</p>
+      <p className='mt-0 mb-50'>No frameworks. Pure vanilla React.</p>
+      <div className='component-wrapper d-flex d-wrap d-x-center d-y-start mb-50 col-gap-x'>
         <Card heading='Counter'>
           <Counter />
         </Card>
@@ -39,11 +36,11 @@ function App() {
           <ImageCarousel />
         </Card>
       </div>
-      <p className="c-light d-flex d-x-center d-y-center">
+      <span className="c-light d-flex d-x-center d-y-center mb-20">
         A project built with 
         <img className='ml-5 mr-5' width="16" height="16" src={lovelogo} alt="filled-like"/>
         by <a className='ml-5' target='_blank' href="https://www.linkedin.com/in/zahinalwa/">Zahin.</a>
-      </p>
+      </span>
     </div>
   )
 }
