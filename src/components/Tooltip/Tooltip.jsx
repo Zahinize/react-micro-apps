04a5166089388f5e2f8d1a721aab60f7aecd47b6
Zahin Omar Alwa <zahin.alwa@gmail.com>
2025-10-08 17:31:56 +0530

    [Code Refactoring] Tooltip

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,9 +1,16 @@
+import { Children, cloneElement } from 'react';
 import './tooltip.css';
 
 export default function Tooltip({ text = "", placement = "top", children }) {
-    return (
-        <span data-text={text} data-placement={placement} className="tip">
-            {children}
-        </span>
-    )
+    const childCount = Children.count(children);
+    if (childCount !== 1) {
+        throw new Error(`<Tooltip> expects exactly one child, but got ${childCount}`);
+    }
+
+    const child = Children.only(children);
+    return cloneElement(child, {
+        className: `${child.props.className || ''} tip`,
+        "data-text": text,
+        "data-placement": placement
+    });
 }
